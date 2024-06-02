32ca3ac4528f8cced85b79fe03111b632fa4ff3e
yassine-turki <turkiyassine2@gmail.com>
2024-06-03 01:48:05 +0200

    Implementation of naive DFT

--- main.cpp
+++ main.cpp
@@ -8,6 +8,8 @@
 #include <sstream>
 #include <algorithm>
 #include <chrono>
+#include <mutex>
+
 
 #define M_PI 3.14159265358979323846
 
@@ -22,6 +24,105 @@ bool is_power_of_two(int n){
 int next_power_of_two(int n){
     int m = std::ceil(std::log2(n));
     return std::pow(2, m);
+}
+
+
+std::vector<complex> Naive_DFT(std::vector<complex> P) {
+    int N = P.size();
+    std::vector<complex> res(N);
+    for (size_t i = 0; i < N; i++) {
+        for (size_t j = 0; j < N; j++) {
+            complex t = std::polar(1.0, -2 * M_PI * i * j / N);
+            res[i] += P[j] * t;
+        }
+    }
+    return res;
+}
+
+std::vector<complex> Naive_IDFT(const std::vector<complex>& P) {
+    int N = P.size();
+    std::vector<complex> res(N);
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            complex t = std::polar(1.0, 2 * M_PI * i * j / N);
+            res[i] += P[j] * t;
+        }
+        res[i] /= N;
+    }
+    return res;
+}
+
+void compute_dft_segment(const std::vector<complex>& P, std::vector<complex>& res, size_t start_index, size_t num_threads, int N, std::mutex& mtx) {
+    for (size_t k = start_index; k < N; k += num_threads) {
+        complex sum = 0;
+        for (size_t j = 0; j < N; ++j) {
+            complex t = std::polar(1.0, -2 * M_PI * j * k / N);
+            sum += P[j] * t;
+        }
+        std::lock_guard<std::mutex> lock(mtx);
+        res[k] = sum;
+    }
+}
+
+
+std::vector<complex> Naive_DFT_Parallel(std::vector<complex> &P, size_t num_threads) {
+    int N = P.size();
+    if (num_threads == 1) {
+        return Naive_DFT(P);
+    }
+
+    std::vector<std::thread> threads;
+    threads.reserve(num_threads - 1);
+
+    std::vector<complex> res(N);
+    std::mutex mtx;
+
+    for (size_t i = 1; i < num_threads; ++i) {
+        threads.emplace_back(compute_dft_segment, std::cref(P), std::ref(res), i, num_threads, N, std::ref(mtx));
+    }
+    compute_dft_segment(P, res, 0, num_threads, N, mtx);
+
+    for (auto& th : threads) {
+        th.join();
+    }
+
+    return res;
+}
+
+void compute_idft_segment(const std::vector<complex>& P, std::vector<complex>& res, size_t start_index, size_t num_threads, int N, std::mutex& mtx) {
+    for (size_t k = start_index; k < N; k += num_threads) {
+        complex sum = 0;
+        for (size_t j = 0; j < N; ++j) {
+            complex t = std::polar(1.0, 2 * M_PI * j * k / N);
+            sum += P[j] * t;
+        }
+        std::lock_guard<std::mutex> lock(mtx);
+        res[k] = sum / (double) N;
+    }
+}
+
+std::vector<complex> Naive_IDFT_Parallel(std::vector<complex>& P, size_t num_threads) {
+    int N = P.size();
+    if (num_threads == 1) {
+        return Naive_IDFT(P);
+    }
+
+    std::vector<std::thread> threads;
+    threads.reserve(num_threads - 1);
+
+    std::vector<complex> res(N);
+    std::mutex mtx;
+
+    for (size_t i = 1; i < num_threads; ++i) {
+        threads.emplace_back(compute_idft_segment, std::cref(P), std::ref(res), i, num_threads, N, std::ref(mtx));
+    }
+    compute_idft_segment(P, res, 0, num_threads, N, mtx);
+
+    for (auto& th : threads) {
+        th.join();
+    }
+
+    return res;
 }
 
 
@@ -335,6 +436,108 @@ std::vector<complex> generate_synthetic_data(int num_points) {
 }
 
 
+/////////////////////////////////////////// Testing functions /////////////////////////////////////////////
+
+std::vector<complex> generate_dummy_data() {
+    return {1, 2, 3, 4};
+}
+
+void test_naive_dft(std::vector<complex> data, int num_threads) {
+    std::vector<complex> result_seq, result_parallel;
+    auto start_seq = std::chrono::system_clock::now();
+
+    result_seq = Naive_DFT(data);
+    result_seq = Naive_IDFT(result_seq);
+
+    auto end_seq = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_seq = end_seq - start_seq;
+    std::cout << "Time taken for Naive serial DFT: " << elapsed_seconds_seq.count() << "s\n";
+
+    auto start_parallel = std::chrono::system_clock::now();
+
+    result_parallel = Naive_DFT_Parallel(data, num_threads);
+    result_parallel = Naive_IDFT_Parallel(result_parallel, num_threads);
+
+    auto end_parallel = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_parallel = end_parallel - start_parallel;
+    std::cout << "Time taken for Naive parallel DFT with " << num_threads << " threads: " << elapsed_seconds_parallel.count() << "s\n";
+
+    // Comparison of Sequential with data
+    bool error = false;
+//    std::cout << "Comparing Naive DFT sequential with data" << std::endl;
+    for (size_t i = 0; i < data.size(); i++) {
+        if (std::abs(result_seq[i] - data[i]) > 1e-6) {
+            std::cout << "Error at index " << i << std::endl;
+            std::cout << "Data: " << data[i] << " Sequential: " << result_seq[i] << std::endl;
+            error = true;
+        }
+    }
+//    if(!error) {
+//        std::cout << "No errors found!" << std::endl;
+//    }
+
+    error = false;
+//    std::cout << "Comparing Naive DFT parallel with data" << std::endl;
+    for (size_t i = 0; i < data.size(); i++) {
+        if (std::abs(result_parallel[i] - data[i]) > 1e-6) {
+        std::cout << "Error at index " << i << std::endl;
+        std::cout << "Data: " << data[i] << " Parallel: " << result_parallel[i] << std::endl;
+        error = true;
+            }
+        }
+//    if(!error) {
+//        std::cout << "No errors found!" << std::endl;
+//    }
+}
+
+void test_radix2(std::vector<complex> data, int num_threads) {
+    std::vector<complex> result_seq;
+    std::vector<complex> result_parallel = data;
+    auto start_seq = std::chrono::system_clock::now();
+
+    result_seq = Radix2FFT(data);
+    result_seq = InverseRadix2FFT(result_seq);
+
+    auto end_seq = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_seq = end_seq - start_seq;
+    std::cout << "Time taken for serial Radix2: " << elapsed_seconds_seq.count() << "s\n";
+
+    auto start_parallel = std::chrono::system_clock::now();
+
+    FFT_Radix2_Parallel(result_parallel, num_threads);
+    Inverse_Radix2_Parallel(result_parallel, num_threads);
+
+    auto end_parallel = std::chrono::system_clock::now();
+    std::chrono::duration<double> elapsed_seconds_parallel = end_parallel - start_parallel;
+    std::cout << "Time taken for parallel Radix2 with " << num_threads << " threads: " << elapsed_seconds_parallel.count() << "s\n";
+
+    // Comparison of Sequential with data
+    bool error = false;
+//    std::cout << "Comparing Radix2 sequential with data" << std::endl;
+    for (size_t i = 0; i < data.size(); i++) {
+        if (std::abs(result_seq[i] - data[i]) > 1e-6) {
+            std::cout << "Error at index " << i << std::endl;
+            std::cout << "Data: " << data[i] << " Sequential: " << result_seq[i] << std::endl;
+            error = true;
+        }
+    }
+//    if(!error) {
+//        std::cout << "No errors found!" << std::endl;
+//    }
+
+    error = false;
+//    std::cout << "Comparing Radix2 parallel with data" << std::endl;
+    for (size_t i = 0; i < data.size(); i++) {
+        if (std::abs(result_parallel[i] - data[i]) > 1e-6) {
+            std::cout << "Error at index " << i << std::endl;
+            std::cout << "Data: " << data[i] << " Parallel: " << result_parallel[i] << std::endl;
+            error = true;
+        }
+    }
+//    if(!error) {
+//        std::cout << "No errors found!" << std::endl;
+//    }
+}
 
 int main() {
 
@@ -350,73 +553,25 @@ int main() {
     else {
 
         bool use_dummy_data = 0; // Test it on {1, 2, 3, 4}
-        bool use_synthetic_data = 1; // Test it on synthetic data (sine)
-        bool use_weather_data = 0; // Test it on weather data from file
+        bool use_synthetic_data = 0; // Test it on synthetic data (sine)
+        bool use_weather_data = 1; // Test it on weather data from file
+        int num_threads = 10;
 
-        if(use_dummy_data){
+        if (use_dummy_data) {
             weather_data = {1, 2, 3, 4};
-        }
-        else if(use_synthetic_data){
+        } else if (use_synthetic_data) {
             weather_data = generate_synthetic_data(10E4);
-        }
-        else if(use_weather_data){
+        } else if (use_weather_data) {
             file_path = "../data/natural_gas_co2_emissions_for_electric_power_sector.csv";
 //            file_path = "../data/Paris_data.csv";
             weather_data = get_weather_data(file_path);
         }
 
-        std::vector<complex> weather_data_parallel = weather_data;
-        std::vector<complex> result_seq, result_parallel = weather_data_parallel;
-
-
-        std::chrono :: time_point<std::chrono::system_clock> start_seq, end_seq, start_parallel, end_parallel;
-        start_seq = std::chrono::system_clock::now();
-        result_seq = Radix2FFT(weather_data);
-
-        // Lines to check IFFT
-        result_seq = InverseRadix2FFT(result_seq);
-
-        end_seq = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds_seq = end_seq - start_seq;
-        std::cout << "Time taken for serial FFT: " << elapsed_seconds_seq.count() << "s\n";
-
-
-        int num_threads = 10;
-        start_parallel = std::chrono::system_clock::now();
-        FFT_Radix2_Parallel(result_parallel, num_threads);
-
-        // Lines to check IFFT
-        Inverse_Radix2_Parallel(result_parallel, num_threads);
-        end_parallel = std::chrono::system_clock::now();
-        std::chrono::duration<double> elapsed_seconds_parallel = end_parallel - start_parallel;
-
-        std::cout << "Time taken for parallel FFT with " << num_threads << " threads: " << elapsed_seconds_parallel.count() << "s\n";
-
-        // Comparison of Sequential and parallel
-        for (int i = 0; i < weather_data.size(); i++) {
-            if (std::abs(result_seq[i] - result_parallel[i]) > 1e-6){
-                std::cout << "Error at index " << i << std::endl;
-                std::cout << "Sequential: " << result_seq[i] << " Parallel: " << result_parallel[i] << std::endl;
-            }
-        }
-        // Comparison of sequential with actual data
-        for (int i = 0; i < weather_data.size(); i++) {
-            if (std::abs(result_seq[i] - weather_data[i]) > 1e-6){
-                std::cout << "Error at index " << i << std::endl;
-                std::cout << "Sequential: " << result_seq[i] << " Actual: " << weather_data[i] << std::endl;
-            }
-        }
-
-        // Comparison of parallel with actual data
-        for (int i = 0; i < weather_data.size(); i++) {
-            if (std::abs(result_parallel[i] - weather_data_parallel[i]) > 1e-6){
-                std::cout << "Error at index " << i << std::endl;
-                std::cout << "Parallel: " << result_parallel[i] << " Actual: " << weather_data_parallel[i] << std::endl;
-            }
-        }
-
-
+        test_naive_dft(weather_data, num_threads);
+        test_radix2(weather_data, num_threads);
     }
+
+
 
     return 0;
 }
