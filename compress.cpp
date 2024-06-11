a68bdfbfb018f84081deaa743b68f7d4654ddbc4
yassine-turki <turkiyassine2@gmail.com>
2024-06-11 12:26:59 +0200

    Pushing compress

--- compress.cpp
+++ compress.cpp
@@ -53,79 +53,30 @@ void write_img(const std::vector<complex> &img_data, int width, int height, cons
 
 
 // Function to compute FFT for rows
-void compute_fft_row(std::vector<complex>& img, int width, int height, int start, int end, int num_sub_threads = 1) {
+void compute_fft_row(std::vector<complex>& img, int width, int height, int start, int end, int num_threads = 1) {
     for (int i = start; i < end; ++i) {
         std::vector<complex> row(img.begin() + i * width, img.begin() + (i + 1) * width);
-        if (num_sub_threads == 1) {
+        if (num_threads == 1) {
             Radix2FFT(row);
         } else {
-            FFT_Radix2_Parallel(row, num_sub_threads);
+            FFT_Radix2_Parallel(row, num_threads);
         }
         std::copy(row.begin(), row.end(), img.begin() + i * width);
     }
 }
 
-// Function to compute FFT for columns
-void compute_fft_column(std::vector<complex>& img, int width, int height, int start, int end, int num_sub_threads = 1) {
-    for (int j = start; j < end; ++j) {
-        std::vector<complex> column(height);
-        std::copy(&img[j], &img[j + height * width], column.begin());
 
-        if (num_sub_threads == 1) {
-            Radix2FFT(column);
-        } else {
-            FFT_Radix2_Parallel(column, num_sub_threads);
-        }
-
-        std::copy(column.begin(), column.end(), &img[j]);
-    }
-}
-//void compute_fft_column(std::vector<complex>& img, int width, int height, int start, int end, int num_sub_threads = 1) {
-//    for (int j = start; j < end; ++j) {
-//        std::vector<complex> column(height);
-//        for (int i = 0; i < height; ++i) {
-//            column[i] = img[i * width + j];
-//        }
-//
-//        if (num_sub_threads == 1) {
-//            Radix2FFT(column);
-//        } else {
-//            FFT_Radix2_Parallel(column, num_sub_threads);
-//        }
-//
-//        for (int i = 0; i < height; ++i) {
-//            img[i * width + j] = column[i];
-//        }
-//    }
-//}
-
-// Function to compute inverse FFT for rows
-void compute_ifft_row(std::vector<complex>& img, int width, int height, int start, int end, int num_sub_threads = 1) {
-    for (int i = start; i < end; ++i) {
-        std::vector<complex> row(img.begin() + i * width, img.begin() + (i + 1) * width);
-
-        if (num_sub_threads == 1) {
-            InverseRadix2FFT(row);
-        } else {
-            Inverse_Radix2_Parallel(row, num_sub_threads);
-        }
-
-        std::copy(row.begin(), row.end(), img.begin() + i * width);
-    }
-}
-
-// Function to compute inverse FFT for columns
-void compute_ifft_column(std::vector<complex>& img, int width, int height, int start, int end, int num_sub_threads = 1) {
+void compute_fft_column(std::vector<complex>& img, int width, int height, int start, int end, int num_threads = 1) {
     for (int j = start; j < end; ++j) {
         std::vector<complex> column(height);
         for (int i = 0; i < height; ++i) {
             column[i] = img[i * width + j];
         }
 
-        if (num_sub_threads == 1) {
-            InverseRadix2FFT(column);
+        if (num_threads == 1) {
+            Radix2FFT(column);
         } else {
-            Inverse_Radix2_Parallel(column, num_sub_threads);
+            FFT_Radix2_Parallel(column, num_threads);
         }
 
         for (int i = 0; i < height; ++i) {
@@ -134,92 +85,50 @@ void compute_ifft_column(std::vector<complex>& img, int width, int height, int s
     }
 }
 
-// Function to compute FFT of an image in parallel
-void parallel_fft_image(std::vector<complex>& img, int width, int height, int num_threads, bool double_parallel = false) {
-    int num_sub_threads = 1;
-    if (double_parallel) {
-        num_threads /= 2;
-        num_sub_threads = 2;
-    }
+// Function to compute inverse FFT for rows
+void compute_ifft_row(std::vector<complex>& img, int width, int height, int start, int end, int num_threads = 1) {
+    for (int i = start; i < end; ++i) {
+        std::vector<complex> row(img.begin() + i * width, img.begin() + (i + 1) * width);
 
-    int block_size = height / num_threads;
-    std::vector<std::thread> threads;
+        if (num_threads == 1) {
+            InverseRadix2FFT(row);
+        } else {
+            Inverse_Radix2_Parallel(row, num_threads);
+        }
 
-    for (int i = 0; i < num_threads - 1; ++i) {
-        int start = i * block_size;
-        int end = (i + 1) * block_size;
-        threads.emplace_back(compute_fft_row, std::ref(img), width, height, start, end, num_sub_threads);
-    }
-    compute_fft_row(img, width, height, (num_threads - 1) * block_size, height, num_sub_threads);
-
-    for (auto& th : threads) {
-        th.join();
-    }
-    threads.clear();
-
-    block_size = width / num_threads;
-    for (int i = 0; i < num_threads - 1; ++i) {
-        int start = i * block_size;
-        int end = (i + 1) * block_size;
-        threads.emplace_back(compute_fft_column, std::ref(img), width, height, start, end, num_sub_threads);
-    }
-    compute_fft_column(img, width, height, (num_threads - 1) * block_size, width, num_sub_threads);
-
-    for (auto& th : threads) {
-        th.join();
+        std::copy(row.begin(), row.end(), img.begin() + i * width);
     }
 }
 
-// Function to compute inverse FFT of an image in parallel
-void parallel_inverse_fft_image(std::vector<complex>& img, int width, int height, int num_threads, bool double_parallel = false) {
-    int num_sub_threads = 1;
-    if (double_parallel) {
-        num_threads /= 2;
-        num_sub_threads = 2;
-    }
+// Function to compute inverse FFT for columns
+void compute_ifft_column(std::vector<complex>& img, int width, int height, int start, int end, int num_threads = 1) {
+    for (int j = start; j < end; ++j) {
+        std::vector<complex> column(height);
+        for (int i = 0; i < height; ++i) {
+            column[i] = img[i * width + j];
+        }
 
-    int block_size = width / num_threads;
-    std::vector<std::thread> threads;
+        if (num_threads == 1) {
 
-    for (int i = 0; i < num_threads - 1; ++i) {
-        int start = i * block_size;
-        int end = (i + 1) * block_size;
-        threads.emplace_back(compute_ifft_column, std::ref(img), width, height, start, end, num_sub_threads);
-    }
-    compute_ifft_column(img, width, height, (num_threads - 1) * block_size, width, num_sub_threads);
+            InverseRadix2FFT(column);
+        } else {
+            Inverse_Radix2_Parallel(column, num_threads);
+        }
 
-    for (auto& th : threads) {
-        th.join();
-    }
-    threads.clear();
-
-    block_size = height / num_threads;
-    for (int i = 0; i < num_threads - 1; ++i) {
-        int start = i * block_size;
-        int end = (i + 1) * block_size;
-        threads.emplace_back(compute_ifft_row, std::ref(img), width, height, start, end, num_sub_threads);
-    }
-    compute_ifft_row(img, width, height, (num_threads - 1) * block_size, height, num_sub_threads);
-
-    for (auto& th : threads) {
-        th.join();
+        for (int i = 0; i < height; ++i) {
+            img[i * width + j] = column[i];
+        }
     }
 }
 
-void fft_image(
-        std::vector<complex> &x, int width, int height,
-        size_t num_sub_threads = 1
-) {
-    compute_fft_row(x, width, height, 0, height, num_sub_threads);
-    compute_fft_column(x, width, height, 0, width, num_sub_threads);
+void compute_fft_img(std::vector<complex> &x, int width, int height, size_t num_threads = 1) {
+    compute_fft_row(x, width, height, 0, height, num_threads);
+    compute_fft_column(x, width, height, 0, width, num_threads);
 }
 
-void inverse_fft_image(
-        std::vector<complex> &x, int width, int height,
-        size_t num_sub_threads = 1
-) {
-    compute_ifft_row(x, width, height, 0, width, num_sub_threads);
-    compute_ifft_column(x, width, height, 0, height, num_sub_threads);
+void compute_ifft_img(std::vector<complex> &x, int width, int height, size_t num_threads = 1) {
+    compute_ifft_row(x, width, height, 0, width, num_threads);
+    compute_ifft_column(x, width, height, 0, height, num_threads);
 }
 
 
@@ -244,15 +153,15 @@ double get_threshold_value(const std::vector<complex>& data, double percentage_t
 }
 
 // Function to compress data using multiple threads
-void compress_block_threads(std::vector<complex>& block, double cut_off, int start, int end) {
+void compress_block_threads(std::vector<complex>& block, double threshold, int start, int end) {
     for (int i = start; i < end; ++i) {
-        if (std::abs(block[i]) < cut_off) {
-            block[i] = 0;
+        if (std::abs(block[i]) < threshold) {
+            block[i] = 0.;
         }
     }
 }
 
-void compress(std::vector<complex>& data, double cut_off, size_t num_threads) {
+void compress(std::vector<complex>& data, double threshold, size_t num_threads) {
     int N = data.size();
     int block_size = N / num_threads;
     std::vector<std::thread> threads;
@@ -260,197 +169,11 @@ void compress(std::vector<complex>& data, double cut_off, size_t num_threads) {
     for (int i = 0; i < num_threads - 1; ++i) {
         int start = i * block_size;
         int end = (i + 1) * block_size;
-        threads.emplace_back(compress_block_threads, std::ref(data), cut_off, start, end);
+        threads.emplace_back(compress_block_threads, std::ref(data), threshold, start, end);
     }
-    compress_block_threads(data, cut_off, (num_threads - 1) * block_size, N);
+    compress_block_threads(data, threshold, (num_threads - 1) * block_size, N);
 
     for (auto& th : threads) {
         th.join();
     }
 }
-//
-//void compute_fft_row(std::vector<complex> &img, int width, int height, int start, int end, int num_sub_threads = 1) {
-//    for (int i = start; i < end; i++) {
-//        std::vector<complex> sliced(width);
-//        for (int j = 0; j < width; j++) {
-//            sliced[j] = img[i * width + j];
-//        }
-//
-//        if (num_sub_threads == 1){
-//            Radix2FFT(sliced);
-//        }
-//        else{
-//            FFT_Radix2_Parallel(sliced, num_sub_threads);
-//        }
-//        for (int k = 0; k < sliced.size(); k++) {
-//            img[i * width + k] = sliced[k];
-//        }
-//    }
-//}
-//
-//void compute_fft_column(std::vector<complex> &img, int width, int height, int start, int end, int num_sub_threads = 1) {
-//    for (int j = start; j < end; j++) {
-//        std::vector<complex> sliced(height);
-//        for (int i = 0; i < height; i++) {
-//            sliced[i] = img[i * width + j];
-//        }
-//        if (num_sub_threads == 1){
-//            Radix2FFT(sliced);
-//        }
-//        else{
-//            FFT_Radix2_Parallel(sliced, num_sub_threads);
-//        }
-//        for (size_t k = 0; k < sliced.size(); k++) {
-//            img[j + k * width] = sliced[k];
-//        }
-//    }
-//}
-//
-//void compute_ifft_row(std::vector<complex> &img, int width, int height, int start, int end, int num_sub_threads = 1) {
-//    for (int i = start; i < end; i++) {
-//        std::vector<complex> sliced(width);
-//        for (int j = 0; j < width; j++) {
-//            sliced[j] = img[i * width + j];
-//        }
-//        if (num_sub_threads == 1){
-//            InverseRadix2FFT(sliced);
-//        }
-//        else{
-//            Inverse_Radix2_Parallel(sliced, num_sub_threads);
-//        }
-//        for (size_t k = 0; k < sliced.size(); k++) {
-//            img[i * width + k] = sliced[k];
-//        }
-//    }
-//}
-//
-//void compute_ifft_column(std::vector<complex> &img, int width, int height, int start, int end, int num_sub_threads = 1) {
-//    for (int j = start; j < end; j++) {
-//        std::vector<complex> sliced(height);
-//        for (int i = 0; i < height; i++) {
-//            sliced[i] = img[i * width + j];
-//        }
-//        if (num_sub_threads == 1){
-//            InverseRadix2FFT(sliced);
-//        }
-//        else{
-//            Inverse_Radix2_Parallel(sliced, num_sub_threads);
-//        }
-//        for (size_t k = 0; k < sliced.size(); k++) {
-//            img[j + k * width] = sliced[k];
-//        }
-//    }
-//}
-//
-//void parallel_fft_image(std::vector<complex> &img, int width, int height, int num_threads, bool double_parallel = false) {
-//    int num_sub_threads = 1;
-//    if (double_parallel) {
-//        num_threads /= 2;
-//        num_sub_threads = 2;
-//    }
-//    int block_size = height / num_threads;
-//    std::vector<std::thread> threads(num_threads - 1);
-//    int start = 0;
-//    int end = block_size;
-//    for (int i = 0; i < (num_threads - 1); i++) {
-//        end = start+block_size;
-//        threads[i] = std::thread(compute_fft_row,ref(img), width, height,start, end, num_sub_threads);
-//        start = end;
-//
-//    }
-//    compute_fft_row(img, width, height, start, height);
-//    for (auto &th : threads) {
-//        th.join();
-//    }
-//    block_size = width / num_threads;
-//    start = 0;
-//    for (int i = 0; i < (num_threads - 1); i++) {
-//        end = start+block_size;
-//        threads[i] = std::thread(compute_fft_column, ref(img), width, height, start, end, num_sub_threads);
-//       start = end;
-//    }
-//    compute_fft_column(img, width, height, start, width);
-//    for (auto &th : threads) {
-//        th.join();
-//    }
-//}
-//
-//void parallel_inverse_fft_image(std::vector<complex> &x, int width, int height, int num_threads, bool double_parallel = false) {
-//    int num_sub_threads = 1;
-//    if (double_parallel) {
-//        num_threads /= 2;
-//        num_sub_threads = 2;
-//    }
-//    int block_size = width / num_threads;
-//    std::vector<std::thread> threads(num_threads - 1);
-//    int start = 0;
-//    int end = block_size;
-//    for (int i = 0; i < (num_threads - 1); i++) {
-//        end = start+block_size;
-//        threads[i] = std::thread(compute_ifft_column,ref(x), width, height,start, end, num_sub_threads);
-//        start = end;
-//    }
-//    compute_ifft_column(x, width, height, start, width);
-//    for (auto &th : threads) {
-//        th.join();
-//    }
-//    block_size = height / num_threads;
-//    start = 0;
-//    for (int i = 0; i < (num_threads - 1); i++) {
-//        end = start+block_size;
-//        threads[i] = std::thread(compute_ifft_row,ref(x), width, height,start, end, num_sub_threads);
-//        start = end;
-//    }
-//    compute_ifft_row(x, width, height, start, height);
-//    for (auto &th : threads) {
-//        th.join();
-//    }
-//}
-//
-//
-//
-//
-//double get_threshold_value(std::vector<std::complex<double>>& data, double percentage_to_keep) {
-//    std::vector<double> abs_data;
-//    for (auto &d : data) {
-//        double abs_value = abs(d);
-//        if (abs_value != 0.0) {  // Skip zero values (in particular because of Radix2)
-//            abs_data.push_back(abs_value);
-//        }
-//    }
-//    if (abs_data.empty()) {
-//        std::cout<< "No data to compress!" << std::endl;
-//        return 0.0;
-//    }
-//    std::sort(abs_data.begin(), abs_data.end());
-//    int index = static_cast<int>((1.0 - percentage_to_keep) * (abs_data.size() ));
-//    index = std::max(0, std::min(index, static_cast<int>(abs_data.size())));
-//    return abs_data[index];
-//}
-//
-//void compress_block_threads(
-//        std::vector<complex> &block, double cut_off, int start, int end
-//) {
-//    for (int i = start; i < end; i++) {
-//        if (abs(block[i]) < cut_off){
-//            block[i] = 0;
-//        }
-//    }
-//}
-//
-//void compress(std::vector<complex> &data, double cut_off, size_t num_threads) {
-//
-//    int N = data.size();
-//    int block_size = N/num_threads;
-//    std::vector<std::thread> threads(num_threads - 1);
-//    int start = 0;
-//    for (int i = 0; i < (num_threads - 1); i++) {
-//        int end = start + block_size;
-//        threads[i] = std::thread(compress_block_threads,std::ref(data), cut_off, start, end);
-//        start += block_size;
-//    }
-//    compress_block_threads(data, cut_off, start, N);
-//    for(auto &th : threads) {
-//        th.join();
-//    }
-//}
